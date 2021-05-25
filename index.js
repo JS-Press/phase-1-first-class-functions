19dde24d368d9da18afa75a5fb2b1475e11186bb
Jarad Solomon <jaradsolomon@gmail.com>
2021-05-25 01:22:19 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,41 @@
+
+
+function receivesAFunction(callback) {
+    callback();
+}
+
+
+
+function returnsANamedFunction(){
+    return function name(){
+
+    }
+}
+
+
+function returnsAnAnonymousFunction(){
+    return () => {};
+}
+
+
+// function callback();
+
+// function aNamedFunction(){
+//     return 'hi';
+// }
+
+
+
+// function returnsANamedFunction(aNamedFunction){
+//     return aNamedFunction();
+// }
+
+
+
+
+
+
+
+// function returnsAnAnonymousFunction(() => 1+1){
+//     return AnAnonymousFunction();
+// }
